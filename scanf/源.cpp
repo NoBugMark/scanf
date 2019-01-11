1af7c6ab46b72609068d86f236215dfe59cc0026
春龙 何 <1397517695@qq.com>
2019-01-11 22:23:23 +0800

    添加项目文件。

---
+++ scanf/源.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SUCURE_NO_WARNING
+#pragma comment(lib, "D://lib/MSVCRTD.Lib")
+
+#include<stdio.h>
+
+int S_1()
+{
+	int a, b, d;
+	char c;
+	scanf("%d%c%d", &a, &c, &b);
+	return d;
+}
+
+int main()
+{
+	S_1();
+
+	return 0;
+}
